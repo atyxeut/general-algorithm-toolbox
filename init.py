198b42538f6574728efefd44e2ca8f8a4f1c0b85
atyxeut <atyxeut@outlook.com>
2026-02-09 01:58:04 +0800

    improve build utils

--- init.py
+++ init.py
@@ -1,28 +1,39 @@
+import argparse
 import shutil
 import subprocess
 from pathlib import Path
 
+# > python3 init.py
+#   by default, specify `my_clang` toolchain
+# > python3 init.py my_gcc
+#   specify `my_gcc` toolchain
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("toolchain", type=str, nargs="?", default="my_clang")
+argv = parser.parse_args()
+
 
 def remove(path: Path):
-    if not path.exists():
-        return
-    if path.is_dir():
-        shutil.rmtree(path)
-    else:
-        path.unlink()
+  if not path.exists():
+    return
+  if path.is_dir():
+    shutil.rmtree(path)
+  else:
+    path.unlink()
 
 
 def main():
-    remove(Path(".cache"))
-    remove(Path(".xmake"))
-    remove(Path("build"))
-    subprocess.run(["xmake", "f"])
-    subprocess.run(
-        ["xmake", "project", "-k", "compile_commands", "--outputdir=build"],
-        text=True,
-        stdout=subprocess.PIPE,
-    )
+  remove(Path(".cache"))
+  remove(Path(".xmake"))
+  remove(Path("build"))
+
+  xmake_toolchain = argv.toolchain
+  subprocess.run(["xmake", "f", "-v", "--toolchain=" + xmake_toolchain])
+  subprocess.run(
+    ["xmake", "project", "-k", "compile_commands", "--outputdir=build"],
+    text=True,
+    stdout=subprocess.PIPE,
+  )
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- xmake.lua
+++ xmake.lua
@@ -1,26 +1,62 @@
 set_xmakever("3.0.6")
-
 set_project("aty-general-algorithm-toolbox")
 
-if is_plat("windows", "linux") then
-    set_toolchains("gcc")
-end
-
-if is_plat("windows") then
-    add_syslinks("stdc++exp")
-end
-
 add_rules("mode.debug", "mode.release")
-
 set_config("builddir", "build")
+set_config("plat", "mingw")
+
 set_languages("c++latest")
 
+local my_gcc_sdk_dir
+if is_host("windows") then
+  -- binary from https://gcc-mcf.lhmouse.com/
+  my_gcc_sdk_dir = "C:/ucrt64"
+end
+
+local my_clang_sdk_dir
+if is_host("windows") then
+  -- binary from https://github.com/mstorsjo/llvm-mingw
+  my_clang_sdk_dir = "C:/llvm-mingw-ucrt-x86_64"
+end
+
+toolchain("my_gcc")
+  set_kind("standalone")
+  set_sdkdir(my_gcc_sdk_dir)
+toolchain_end()
+
+toolchain("my_clang")
+  set_kind("standalone")
+  set_sdkdir(my_clang_sdk_dir)
+  set_toolset("cxx", "clang", "clang++")
+  set_toolset("ld", "clang++", "clang")
+toolchain_end()
+add_cxxflags("my_clang::-stdlib=libc++")
+
+if get_config("toolchain") == "my_clang" then
+  set_toolchains("my_clang")
+  target("std_modules")
+    set_kind("object")
+    add_includedirs(my_clang_sdk_dir .. "/include/c++/v1")
+    add_files(my_clang_sdk_dir .. "/share/libc++/v1/*.cppm", {public = true})
+  target_end()
+elseif get_config("toolchain") == "my_gcc" then
+  set_toolchains("gcc")
+  if is_host("windows") then
+    add_syslinks("stdc++exp") -- gcc16 still requires this on Windows
+  end
+end
+
 target("gat")
-    set_kind("shared")
-    add_files("src/**.cppm", {public = true})
+  set_kind("object")
+  if get_config("toolchain") == "my_clang" then
+    add_deps("std_modules")
+  end
+  add_files("src/**.cppm", {public = true})
+target_end()
 
 target("test")
-    set_kind("binary")
-    add_deps("gat")
-    add_files("test.cpp")
-    set_optimize("faster")
+  set_kind("binary")
+  add_deps("gat")
+  add_files("test.cpp")
+  set_optimize("faster")
+target_end()
