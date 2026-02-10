ffbb238f2c1e0647eee2422601745d1a305c637b
atyxeut <atyxeut@outlook.com>
2026-02-10 21:00:06 +0800

    improve build utils: maybe the right way to config llvm toolchain

--- xmake.lua
+++ xmake.lua
@@ -1,36 +1,23 @@
 set_xmakever("3.0.6")
 set_project("aty-general-algorithm-toolbox")
+set_languages("c++latest")
 
 add_rules("mode.debug", "mode.release")
 
 set_config("builddir", "build")
-
-if is_host("window") then
-  -- recommended gcc binary: https://gcc-mcf.lhmouse.com/
+set_config("runtimes", "c++_shared")
+if is_host("windows") then
   set_config("plat", "mingw")
 end
 
-set_languages("c++latest")
-
-if get_config("toolchain") == "llvm" then
-  if is_host("windows") then
-    -- binary from https://github.com/mstorsjo/llvm-mingw
-    local llvm_sdk_dir = "C:/llvm-mingw-ucrt-x86_64"
-    -- suppress `warning: std and std.compat modules not found! maybe try to add --sdk=<PATH/TO/LLVM> or install libc++`
-    set_policy("build.c++.modules.std", false)
-
-    target("std_modules")
-      set_kind("object")
-      add_includedirs(llvm_sdk_dir .. "/include/c++/v1")
-      add_files(llvm_sdk_dir .. "/share/libc++/v1/*.cppm", { public = true })
-    target_end()
-  elseif is_host("macosx") then
-    set_policy("build.c++.modules", true)
-    -- binary from Homebrew: brew install llvm
-    add_ldflags("-L/usr/local/opt/llvm@21/lib/c++")
-  end
+-- recommended llvm binary: https://github.com/mstorsjo/llvm-mingw
+if get_config("toolchain") == "llvm" and is_host("macosx") then
+  set_policy("build.c++.modules", true)
+  -- binary from Homebrew: brew install llvm
+  add_ldflags("-L/usr/local/opt/llvm@21/lib/c++")
 end
 
+-- recommended gcc binary: https://gcc-mcf.lhmouse.com/
 if get_config("toolchain") == "gcc" and is_host("windows") then
   -- gcc 16 still requires this on Windows
   add_syslinks("stdc++exp")
@@ -38,9 +25,6 @@ end
 
 target("gatk")
   set_kind("object")
-  if get_config("toolchain") == "llvm" and is_host("windows") then
-    add_deps("std_modules")
-  end
   add_files("src/**.cppm", { public = true })
 target_end()
 
